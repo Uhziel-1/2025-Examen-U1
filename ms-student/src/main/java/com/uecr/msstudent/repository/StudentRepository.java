655cf02dc726da6d0251a237f9172515895a4828
Uhziel-1 <uhzielcreciendo@gmail.com>
2025-04-23 07:52:05 -0500

    First Commit

--- ms-student/src/main/java/com/uecr/msstudent/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.uecr.msstudent.repository;
-
-import com.uecr.msstudent.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
